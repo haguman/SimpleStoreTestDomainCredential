bd2c7f36fc1f054b7dd178d28af197bc082f5b0f
haguman <haguman@hotmail.com>
2019-12-28 02:13:43 +0100

    Add files via upload

---
+++ simplecredentialstoreandtest.ps1
@@ -0,0 +1,5 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement") | Out-Null
+$username = Read-Host -Prompt "Please enter your username : "
+$netbios_domain = Read-Host -Prompt "Please enter netbios domain  : "
+$domaincred = New-Object ûTypeName "System.Management.Automation.PSCredential" ûArgumentList ($netbios_domain+"\"+$mgmtdwrusername), $(Read-Host -Prompt "Please enter your password : " -AsSecureString)
+$domaincred | %{"password correct ? " + (New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain, $($_.GetNetworkCredential().Domain ) ) ).ValidateCredentials($_.GetNetworkCredential().UserName, $_.GetNetworkCredential().Password).ToString()}
